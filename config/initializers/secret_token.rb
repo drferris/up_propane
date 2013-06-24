93cd5733902f923b4ef0b972fc3e6173475183f6
Dave Ferris <dave@entrision.com>
2013-06-24 16:05:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpPropane::Application.config.secret_token = 'fa359d42177d19890ef9f91ff474eef79693874db57b8dbbdff60cdac2f65bebbfd1616c2dd0fce0bff140d74228835ec954564c4a96aaa4a4c8975ea5a44c5b'
